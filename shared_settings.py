d8b879910a379ca1341774bbdd0a486669f3e5df
Torben Laursen <tl@vlxe.com>
2023-09-30 18:32:08 +0200

    Updated settings

--- shared_settings.py
+++ shared_settings.py
@@ -3,6 +3,6 @@
 
 class sharedsettings:
 
-    user_id = "9DABA1C2-ACDA-47DB-AACD-593F720AFF58" # Change to value provided by VLXE
-    access_secret = "O2(YsDf&wHTOSAbviFkdP>*C?593y3#ee80%!T4HK;2)5>0A2h" # Change to value provided by VLXE
-    url = "https://localhost:5201"
+    user_id = "" # Change to value provided by VLXE
+    access_secret = "" # Change to value provided by VLXE
+    url = "https://api.equia.com"
